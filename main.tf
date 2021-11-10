14462e4178558a5763363b171af0f50963fce100
deepakvermacloud <deepak.verma@clouddrove.com>
2021-11-10 14:57:22 +0530

    label module removed

--- main.tf
+++ main.tf
@@ -1,13 +1,5 @@
-module "labels" {
-  source  = "clouddrove/labels/aws"
-  version = "0.15.0"
-
-  name        = var.name
-  repository  = var.repository
-  environment = var.environment
-  managedby   = var.managedby
-  label_order = var.label_order
-}
+data "aws_partition" "security_hub" {}
+data "aws_region" "security_hub" {}
 
 locals {
   enabled_standards_arns = toset([
@@ -21,8 +13,6 @@ locals {
   ])
 }
 
-data "aws_partition" "security_hub" {}
-data "aws_region" "security_hub" {}
 
 resource "aws_securityhub_account" "security_hub" {
   count = var.security_hub_enabled ? 1 : 0
