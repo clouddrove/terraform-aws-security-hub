1a087058da1fbd1faca8ee5d5260ce925feac934
ruchit-sharma09 <ruchit092003@gmail.com>
2025-12-12 22:52:43 +0530

    Added some controls arn for cspm enabling

--- examples/example.tf
+++ examples/example.tf
@@ -11,21 +11,25 @@ module "security-hub" {
   #member account add
   member_details = [
     {
-      account_id = "1122334466"
+      account_id = "924144197303"
       mail_id    = "hello@clouddrove.com"
       invite     = true
     },
-    {
-      account_id = "1122334455"
-      mail_id    = "temp@clouddrove.com"
-      invite     = true
-    }
+    # {
+    #   account_id = "924144197303"
+    #   mail_id    = "temp@clouddrove.com"
+    #   invite     = true
+    # }
   ]
 
   #standards 
   enabled_standards = [
     "standards/aws-foundational-security-best-practices/v/1.0.0",
-    "ruleset/cis-aws-foundations-benchmark/v/1.2.0"
+    "ruleset/cis-aws-foundations-benchmark/v/1.2.0",
+    "aws-foundational-security-best-practices/v/1.0.0",
+    "cis-aws-foundations-benchmark/v/1.2.0",
+    "nist-800-53/v/5.0.0",
+    "pci-dss/v/4.0.1"
   ]
 
   #products
